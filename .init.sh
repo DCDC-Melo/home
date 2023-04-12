cfc0fdaa3f9e55d423a5790b52e549776f3e0b4c
melo.wang <melo.wang@bybit.com>
2023-04-12 19:50:13 +0800

    dc

--- .init.sh
+++ .init.sh
@@ -1,3 +1,10 @@
 brew install gpg
 gpg --list-keys
 curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.dc.private.key.gpg | tee $(mktemp) && gpg --import $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.zshenv.gpg | tee $(mktemp) && source $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.zshrc.gpg | tee $(mktemp) && source $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.shhi.gpg | tee $(mktemp) && fc -R $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.shhm.gpg | tee $(mktemp) && fc -R $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.shhs.gpg | tee $(mktemp) && fc -R $_
+curl https://raw.githubusercontent.com/DCDC-Melo/home/main/.zsh_history.gpg | tee $(mktemp) && fc -R $_
+
